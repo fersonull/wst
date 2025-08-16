5a526a5c8f5dbe729caa32a01a2429ac46c7e4e6
Casper <culaspero@gmail.com>
2025-08-16 09:42:25 +0800

    remove the dark schema

--- src/Pages/Layouts/MainLayout.jsx
+++ src/Pages/Layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import NavBar from "../../Components/ui/NavBar";
 
 const MainLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen dark:text-white font-geist dark:bg-[#171717]">
+    <div className="flex flex-col min-h-screen text-white font-geist bg-[#171717]">
       <NavBar />
 
       <div className="h-full mx-auto lg:w-[65%] w-[90%]">
